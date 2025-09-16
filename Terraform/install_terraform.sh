ac56fcf5f55445cc37b333df3ba1cee88bd73415
nerdflash28 <nerdflash30@gmail.com>
2025-09-16 08:10:42 +0000

    Terraform repo added

---
+++ Terraform/install_terraform.sh
@@ -0,0 +1,12 @@
+# Installation
+* in order to run terraform we only need to download single binary file 
+
+link: terraform.io/downloads
+
+## installation via package manager
+```bash
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo yum -y install terraform
+```
+
